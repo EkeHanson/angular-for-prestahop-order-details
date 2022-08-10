93a36407add3c897f2dc4dd26119e1e9a2443aee
ekenehanson <ekenehanson@gmail.com>
2022-08-10 15:01:39 +0100

    first commit

---
+++ UdemyCodePractices/Modal/script.js
@@ -0,0 +1,57 @@
+let js ="amazing";
+if(js === "amazing") alert("JavaScript is fun");
+
+console.log(40+800+50);
+
+console.log("Jonas");
+console.log(23);
+let firsName = "Ekene-onwon";
+let lastName = "Abraham";
+console.log(firsName);
+console.log(lastName);
+console.log(true);
+console.log(typeof true);
+console.log(typeof 'Jonas');
+console.log(typeof 23);
+let year;
+console.log(typeof year);
+
+year = 1991;
+
+console.log(typeof year);
+console.log(typeof null);
+// variables can be initialize without declaration
+_year1 = 1995;
+// 3 ways of declaring variables
+let age = 30;
+//reaasigning
+age = 35;
+let _year;
+
+// For variables that will not change (Immuttable) and not null
+const birthYear = 1997
+
+
+//var is the old way of declaring variables do not use
+var job = "programmer";
+job = 'Teacher';
+ const now = 2022;
+const ageJonas = now -1997;
+
+const agesarah = now -1997;
+console.log(ageJonas, ageJonas);
+
+console.log(ageJonas*12);
+console.log(2**3);
+console.log(now - ageJonas > now - agesarah);
+
+let dink2; 
+
+if(age >=18){
+    drink2 = 'Wine';
+}drink2 = 'water';
+
+console.log(drink2);
+
+
+
